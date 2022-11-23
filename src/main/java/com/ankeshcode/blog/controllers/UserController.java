9f81fe2aa33cb5de8543305801c0034baf1039dc
Ankesh <ankeshpatil156@gmail.com>
2022-11-23 15:52:37 +0530

    4th commit

--- src/main/java/com/ankeshcode/blog/controllers/UserController.java
+++ src/main/java/com/ankeshcode/blog/controllers/UserController.java
@@ -1,8 +1,13 @@
 package com.ankeshcode.blog.controllers;
 
+import com.ankeshcode.blog.payloads.UserDto;
 import com.ankeshcode.blog.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/api/users")
@@ -12,6 +17,12 @@ public class UserController {
 	private UserService userService;
 
 	//POST :to create user
+@PostMapping
+public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
+	UserDto createUserDto=this.userService.createUser(userDto);
+	return new ResponseEntity<>(createUserDto,HttpStatus.CREATED);
+	
+}
 	//PUT : To update user
 	//DELETE : To Delete user
 	//GET : To get user
